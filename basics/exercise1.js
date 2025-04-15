19bf584a02a9b8629d9a1d464f5e9783b312283c
Kelechi Otugbuali <hilakechi@gmail.com>
2025-04-15 11:34:42 +0100

    Exercise 1 done

---
+++ basics/exercise1.js
@@ -0,0 +1,27 @@
+function checkEvenOrOdd() {
+  const number = getUserInput();
+
+  if (isValidNumber(number)) {
+    const num = parseInt(number, 10);
+    const result = isEven(num) ? 'even' : 'odd';
+    showResult(num, result);
+  } else {
+    alert('Please enter a valid number.');
+  }
+}
+
+function getUserInput() {
+  return prompt('Please enter a number:');
+}
+
+function isValidNumber(number) {
+  return number !== null && !isNaN(number);
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function showResult(number, result) {
+  alert(`The number ${number} is ${result}.`);
+}
